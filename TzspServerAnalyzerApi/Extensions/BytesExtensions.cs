1161099aedc1efb7daf9bb3d8b4c2dd0480789fc
Maksim Kislyakov <mail@kmaksim.dev>
2020-03-08 20:34:29 +0300

    Add AsAsciiLetters method that returns ASCII representation of byte[]

--- TzspServerAnalyzerApi/Extensions/BytesExtensions.cs
+++ TzspServerAnalyzerApi/Extensions/BytesExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Text;
 
 namespace TzspServerAnalyzerApi.Extensions
@@ -6,15 +8,24 @@ namespace TzspServerAnalyzerApi.Extensions
     {
         private static readonly string[] _lowerHex;
         private static readonly string[] _upperHex;
+        private static readonly char[] _asciiLetters;
 
         static BytesExtensions()
         {
             _lowerHex = new string[(int)byte.MaxValue + 1];
             _upperHex = new string[(int)byte.MaxValue + 1];
+            _asciiLetters = new char[(int)byte.MaxValue + 1];
+
             for(int i = 0; i <= byte.MaxValue; ++i)
             {
                 _lowerHex[i] = i.ToString("x2").ToLowerInvariant();
                 _upperHex[i] = i.ToString("x2").ToUpperInvariant();
+
+                char ch = Encoding.ASCII.GetChars(new byte[] { (byte)i }).Single();
+                if (!Char.IsControl(ch))
+                    _asciiLetters[i] = ch;
+                else
+                    _asciiLetters[i] = '.';
             }
         }
 
@@ -30,6 +41,13 @@ namespace TzspServerAnalyzerApi.Extensions
             if (bytes == null)
                 return string.Empty;
             return AsHexUpper(bytes, 0, bytes.Length);
+        }
+
+        public static string AsAsciiLetters(this byte[] bytes)
+        {
+            if (bytes == null)
+                return string.Empty;
+            return AsAsciiLetters(bytes, 0, bytes.Length);
         }
 
         public static string AsHexLower(this byte[] bytes, int start, int length)
@@ -51,6 +69,17 @@ namespace TzspServerAnalyzerApi.Extensions
             var builder = new StringBuilder(length * 2);
             for (int i = start; i < length; ++i)
                 builder.Append(_upperHex[bytes[i]]);
+            return builder.ToString();
+        }
+
+        public static string AsAsciiLetters(this byte[] bytes, int start, int length)
+        {
+            if (bytes == null || bytes.Length == 0)
+                return string.Empty;
+
+            var builder = new StringBuilder(length);
+            for (int i = start; i < length; ++i)
+                builder.Append(_asciiLetters[bytes[i]]);
             return builder.ToString();
         }
     }
